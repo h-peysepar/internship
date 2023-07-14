617ed4a6cfb5a1d6636609df74e3591c3519e55e
HadiPr <hadipeysepar79@gmail.com>
2023-07-14 18:42:51 +0330

    refactor code

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 import pandas as pd
 from tabulate import tabulate
+from utility import calculate_columns, calculate_max_productivity, create_dataframe
 
-df = pd.read_excel('data.xlsx', skiprows=16, nrows=17, usecols="L:L")
-df.columns = ['Efficiency']
-df = df[df['Efficiency'] > 0]
-last8YearEfficiency = df['Efficiency'].mean()
-print('Crop Efficiency In Last 8 Years:', last8YearEfficiency)
+df = create_dataframe()
+df['Irrig. Scheduling F=10'] = df['IR']
 
 
-df = pd.read_excel('data.xlsx', skiprows=102, nrows=23, usecols="A:I")
-print(df)
-df.columns = ['Month', 'decade', 'progress level', 'Kc', 'ETo', 'ETc', 'Pe', 'IR', 'Irrig. Scheduling F=10']
-
-df['Raes Method1'] = .5 * (df['Irrig. Scheduling F=10'] / df['IR'])
-df['Raes Method2'] = 1 - df['Raes Method1']
-df['Raes Method3'] = pow(df['Raes Method2'], 10/220)
-df['Raes Method4'] = 1 - df['Raes Method3']
-df['Irrig. Scheduling F=30'] = None
-
-for i in range(0, len(df), 3):
-    df.loc[i, 'Irrig. Scheduling F=30'] = ''
-    df.loc[i+2, 'Irrig. Scheduling F=30'] = ''
-    df.loc[i+1, 'Irrig. Scheduling F=30'] = df.loc[i:i + 2, 'Irrig. Scheduling F=10'].sum()
-
+calculate_columns(df)
 
 table = tabulate(df, headers=df.columns, tablefmt='outline')
 print(table)
---
+++ utility.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+def calculate_max_productivity():
+    df = pd.read_excel('data.xlsx', skiprows=16, nrows=17, usecols="L:L")
+    df.columns = ['Efficiency']
+    df = df[df['Efficiency'] > 0]
+    array = df['Efficiency'].values
+    last8YearEfficiency = df['Efficiency'].mean()
+    # print('Crop Efficiency In Last 8 Years:', last8YearEfficiency)
+    maxCropEfficiency = df['Efficiency'].max()
+
+    return maxCropEfficiency
+
+
+def create_dataframe():
+    df = pd.read_excel('data.xlsx', skiprows=102, nrows=23, usecols="A:H")
+    # print(df)
+    df.columns = ['Month', 'decade', 'progress level', 'Kc', 'ETo', 'ETc', 'Pe', 'IR']
+    return df
+
+def calculate_columns(df):
+    df['Raes Method1'] = .5 * (df['Irrig. Scheduling F=10'] / df['IR'])
+    df['Raes Method2'] = 1 - df['Raes Method1']
+    df['Raes Method3'] = pow(df['Raes Method2'], 10/220)
+    df['Raes Method4'] = 1 - df['Raes Method3']
+    df['Irrig. Scheduling F=30'] = None
+
+    for i in range(0, len(df), 3):
+        df.loc[i, 'Irrig. Scheduling F=30'] = ''
+        df.loc[i+2, 'Irrig. Scheduling F=30'] = ''
+        df.loc[i+1, 'Irrig. Scheduling F=30'] = df.loc[i:i +
+                                                    2, 'Irrig. Scheduling F=10'].sum()
+
+
+
+    # real_productivity =
+    # return {'real_productivity': }
