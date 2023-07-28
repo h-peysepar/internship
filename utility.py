8395915dcf02ff2945fe81835d256154dad67e17
HadiPr <hadipeysepar79@gmail.com>
2023-07-28 10:37:23 +0330

    renaming and change excel format

--- utility.py
+++ utility.py
@@ -1,38 +1,47 @@
 import pandas as pd
 from math import prod
 
+def read_input(cellName):
+    df = pd.read_excel('data.xlsx', sheet_name='Input', skiprows=1, nrows=3, usecols="A:B")
+    df.columns = ['key', 'value']
+    key_row = df[df['key'] == cellName]
+    
+    if not key_row.empty:
+        value = key_row.iloc[0]['value']
+        return value
+    else:
+        return None
 
 
 def get_expected_productivity():
-    return 5000
+    return read_input('Expected Crop Yeild')
 
 
 def get_max_productivity():
-    df = pd.read_excel('data.xlsx', skiprows=16, nrows=17, usecols="L:L")
+    df = pd.read_excel('data.xlsx', sheet_name='Crop History', skiprows=1, nrows=9, usecols="B:B")
     df.columns = ['Efficiency']
-    df = df[df['Efficiency'] > 0]
-    array = df['Efficiency'].values
-    last8YearEfficiency = df['Efficiency'].mean()
     maxCropEfficiency = df['Efficiency'].max()
     return maxCropEfficiency
 
 
 def create_dataframe():
-    df = pd.read_excel('data.xlsx', skiprows=102, nrows=23, usecols="A:H")
-    df.columns = ['Month', 'decade', 'progress level',
-                  'Kc', 'ETo', 'ETc', 'Pe', 'IR']
+    df = pd.read_excel('data.xlsx', sheet_name='Irrigation', skiprows=1, nrows=23, usecols="A:H")
+    df.columns = ['Month', 'Decade', 'Progress Stage', 'Kc', 'ETo', 'ETc', 'Effective Precipitation', 'Irrigation']
     return df
 
 
 def calculate_columns(df):
-    df['Raes Method1'] = df['progress level'] * \
-        (df['Irrig. Scheduling F=10'] / df['IR'])
+    df['Raes Method1'] = df['Progress Stage'] * (df['Irrigation Scheduling'] / df['Irrigation'])
     df['Raes Method2'] = 1 - df['Raes Method1']
     df['Raes Method3'] = pow(df['Raes Method2'], 10/220)
     df['Raes Method4'] = 1 - df['Raes Method3']
-    df['Irrig. Scheduling F=30'] = None
-    # for i in range(0, len(df), 3):
-    #     df.loc[i, 'Irrig. Scheduling F=30'] = ''
-    #     df.loc[i+2, 'Irrig. Scheduling F=30'] = ''
-    #     df.loc[i+1, 'Irrig. Scheduling F=30'] = df.loc[i:i +
-    #                                                    2, 'Irrig. Scheduling F=10'].sum()
+    cycle = read_input('Irrigation Frequency')
+    cycle_key = f'Irrigation Scheduling F={cycle}'
+    df[cycle_key] = None
+    jump = cycle / 10
+    for i in range(0, len(df)):
+        if (i % jump) == 0:
+            df.loc[i, cycle_key] = df.loc[i : i + jump - 1, 'Irrigation Scheduling'].sum()
+
+
+
